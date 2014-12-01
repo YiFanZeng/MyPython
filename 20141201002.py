33f3e92bbfcc77a875b6b9bdf0ce0e42003e20e1
Michael <Michael@Mchael.zeng.com>
2014-12-01 10:47:08 +0800

    Unicode. Signed-off-by: Michael <Michael@Mchael.zeng.com>

---
+++ 20141201002.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print ord('A')
+print chr(65)
+print u'中文'
+print u'中'
+print u'ABC'.encode('utf-8')
+print u'中文'.encode('utf-8')
+print 'abc'.decode('utf-8')
+'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+print '\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+

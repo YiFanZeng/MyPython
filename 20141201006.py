c9b841452852300060f92dfa410e0b244da50532
Michael <Michael@Mchael.zeng.com>
2014-12-01 12:06:47 +0800

    call function. Signed-off-by: Michael <Michael@Mchael.zeng.com>

---
+++ 20141201006.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# abs()
+print 'abs(100) = ', abs(100)
+print 'abs(-100) = ', abs(-100)
+# cmp()
+print 'cmp(1, 2)', cmp(1, 2)
+print 'int(12.34)', int(12.34)
+print "float('12.34')", float('12.34')
+print 'str(1.23)', str(1.23)

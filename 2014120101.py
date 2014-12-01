3b5e9fc91e15ea3a11162588b2a3e08c06fd90b9
Michael <Michael@Mchael.zeng.com>
2014-12-01 10:16:35 +0800

    output and input. Signed-off-by: Michael <Michael@Mchael.zeng.com>

---
+++ 2014120101.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+print '======output======'
+print 200 + 100
+print '200 + 100 =', 200 + 10
+print '======input======'
+name = raw_input('Please enter your name: ')
+print 'hello, ', name
+
